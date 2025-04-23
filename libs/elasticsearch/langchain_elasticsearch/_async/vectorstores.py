696767ca5a250f6179374312a2365b9d48d1de95
Sam <s.bartlett@slcyber.io>
2025-04-23 11:58:13 +0100

    add `fields` to all search methods

--- libs/elasticsearch/langchain_elasticsearch/_async/vectorstores.py
+++ libs/elasticsearch/langchain_elasticsearch/_async/vectorstores.py
@@ -389,6 +389,7 @@ class AsyncElasticsearchStore(VectorStore):
         k: int = 4,
         fetch_k: int = 50,
         filter: Optional[List[dict]] = None,
+        fields: Optional[List[str]] = None,
         *,
         custom_query: Optional[
             Callable[[Dict[str, Any], Optional[str]], Dict[str, Any]]
@@ -414,11 +415,13 @@ class AsyncElasticsearchStore(VectorStore):
             num_candidates=fetch_k,
             filter=filter,
             custom_query=custom_query,
+            fields=fields,
         )
         docs = _hits_to_docs_scores(
             hits=hits,
             content_field=self.query_field,
             doc_builder=doc_builder,
+            fields=fields,
         )
         return [doc for doc, _score in docs]
 
@@ -504,6 +507,7 @@ class AsyncElasticsearchStore(VectorStore):
         query: str,
         k: int = 4,
         filter: Optional[List[dict]] = None,
+        fields: Optional[List[str]] = None,
         *,
         custom_query: Optional[
             Callable[[Dict[str, Any], Optional[str]], Dict[str, Any]]
@@ -528,12 +532,13 @@ class AsyncElasticsearchStore(VectorStore):
             raise ValueError("scores are currently not supported in hybrid mode")
 
         hits = await self._store.search(
-            query=query, k=k, filter=filter, custom_query=custom_query
+            query=query, k=k, filter=filter, custom_query=custom_query, fields=fields
         )
         return _hits_to_docs_scores(
             hits=hits,
             content_field=self.query_field,
             doc_builder=doc_builder,
+            fields=fields,
         )
 
     async def asimilarity_search_by_vector_with_relevance_scores(
@@ -541,6 +546,7 @@ class AsyncElasticsearchStore(VectorStore):
         embedding: List[float],
         k: int = 4,
         filter: Optional[List[Dict]] = None,
+        fields: Optional[List[str]] = None,
         *,
         custom_query: Optional[
             Callable[[Dict[str, Any], Optional[str]], Dict[str, Any]]
@@ -570,10 +576,12 @@ class AsyncElasticsearchStore(VectorStore):
             k=k,
             filter=filter,
             custom_query=custom_query,
+            fields=fields,
         )
         return _hits_to_docs_scores(
             hits=hits,
             content_field=self.query_field,
+            fields=fields,
             doc_builder=doc_builder,
         )
 
